987a30457229b8de3a1a35bc48d2fdd955c22c16
Reaper Gelera <ahoy@barelyhuman.dev>
2024-07-21 12:43:19 +0530

    chore: remove jsdoc types

--- adex/src/hook.js
+++ adex/src/hook.js
@@ -5,26 +5,16 @@ export const CONSTANTS = {
   apiCall: Symbol('api-call'),
 }
 
-/**
- * @param {string|symbol} eventName
- * @param {(data:any)=>void|Promise<void>} handler
- */
 export function hook(eventName, handler) {
   const handlers = hookListeners.get(eventName) || []
   handlers.push(handler)
   hookListeners.set(eventName, handlers)
 }
 
-/**
- * @param {(ctx: Context)=> void} fn
- */
 export async function beforePageRender(fn) {
   hook(CONSTANTS.pageRender, fn)
 }
 
-/**
- * @param {(ctx: APIContext)=> void} fn
- */
 export async function beforeAPICall(fn) {
   hook(CONSTANTS.apiCall, fn)
 }
