2dd82f7e1a9b889cb90c2e164fa564468d037d1a
Siddharth Gelera (reaper) <ahoy@barelyhuman.dev>
2025-01-02 18:18:33 +0530

    fix: fonts.css isn't loaded in build mode (#21)

--- adex/src/ssr.d.ts
+++ adex/src/ssr.d.ts
@@ -1,3 +1,4 @@
 export { toStatic } from 'hoofd/preact'
 export { renderToString } from 'preact-render-to-string'
 export { parse as pathToRegex } from 'regexparam'
+export { use as useMiddleware } from '@barelyhuman/tiny-use'
