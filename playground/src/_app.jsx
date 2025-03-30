d97d39635dfd7e4cf0bcfc719a38b6e5da5efdd7
reaper <ahoy@barelyhuman.dev>
2025-03-30 20:16:43 +0530

    chore: typo in release

--- playground/src/_app.jsx
+++ playground/src/_app.jsx
@@ -1,18 +1,13 @@
 import { App as AdexApp } from 'adex/app'
 import { h } from 'preact'
 import { hydrate as preactHydrate } from 'adex/router'
-import { ThemeProvider } from '@preachjs/themes'
 
 export { prerender } from 'adex/app'
 
 import 'virtual:adex:global.css'
 
 export const App = ({ url }) => {
-  return (
-    <ThemeProvider>
-      <AdexApp url={url} />
-    </ThemeProvider>
-  )
+  return <AdexApp url={url} />
 }
 
 async function hydrate() {
